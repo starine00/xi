f53a481e33c2a916f99ce571aafefd65dd001eb3
starine00 <72542858+starine00@users.noreply.github.com>
2021-06-13 22:41:53 +0800

    Add files via upload

---
+++ 6_13zhizhen.cpp
@@ -0,0 +1,93 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//
+//	//int *p=NULL;//p是整形指针-指向整形地址-可以存放整形地址
+//	//char* pc=NULL;//pc是字符指针-指向字符地址-可以存放字符的地址
+//					//数组指针-指向数组的指针-可以存放数组的地址
+//	//int arr[10]={0};
+//	//arr-首元素的地址
+//	//&arr[0]-首元素的地址
+//	//&arr-数组的地址
+//	//int arr[10]={1,2,3,4,5,6,7,8,9,0};
+//	//int (*p)[10]=&arr;//数组的地址要存起来
+//	//上面的p就是数组指针。
+//	int arr[10]={1,2,3,4,5,6,7,8,9,0};
+//	int* p=arr;
+//	int i=0;
+//	for(i=0;i<10;i++)
+//	{
+//		printf("%d\n",*(p+i));
+//	}
+//	//int (*pa)[10]=&arr;
+//	//int i=0;
+//	//for(i=0;i<10;i++)
+//	//{
+//	//	/*printf("%d\n",(*pa)[i]);*/
+//	//	printf("%d\n",*((*pa)+i));
+//	//}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void printf1(int arr[3][5],int x,int y)
+//{
+//	int i=0;
+//	int j=0;
+//	for(i=0;i<3;i++)
+//	{
+//		for(j=0;j<5;j++)
+//		{
+//			printf("%d ",arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void printf2(int (*p)[5],int x,int y)
+//{
+//	int i=0;
+//	for(i=0;i<3;i++)
+//	{
+//		int j=0;
+//		for(j=0;j<5;j++)
+//		{
+//			printf("%d ",*(*(p+i)+j));
+//		}
+//		printf("\n");
+//	}
+//	
+//}
+//int main()
+//{
+//	int arr[3][5]={{1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7}};
+//	printf1(arr,3,5);
+//	printf2(arr,3,5);
+//	system("pause");
+//	return 0;
+//}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int arr[10]={1,2,3,4,5,6,7,8,9,0};
+	int i=0;
+	int* p=arr;
+	for(i=0;i<10;i++)
+	{
+	printf("%d ",arr[i]);
+	printf("%d ",*(p+i));
+	printf("%d ",*(arr+i));
+	printf("%d ",p[i]);
+	}
+	
+	system("pause");
+	return 0;
+}
